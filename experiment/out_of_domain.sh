adcf8fdd258f58934facbaba0677856f0d62f855
tomotomonakanaka <tomohiro9261@gmail.com>
2021-11-13 10:24:11 +0900

    change error

--- experiment/out_of_domain.sh
+++ experiment/out_of_domain.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # out-of-domain
-python test_embeddings.py --relu --seed 1 --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
+python out_of_domain.py --relu --seed 1 --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
 
 for i in 2 3 4 5
 do
-python test_embeddings.py --relu --use_saved_data --seed $i --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
+python out_of_domain.py --relu --use_saved_data --seed $i --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
 done
 
 # out-of-domain normalize
-python test_embeddings.py --normalize --relu --seed 1 --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
+python out_of_domain.py --normalize --relu --seed 1 --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
 
 for i in 2 3 4 5
 do
-python test_embeddings.py --normalize --relu --use_saved_data --seed $i --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
+python out_of_domain.py --normalize --relu --use_saved_data --seed $i --task_targeted delaney+sampl+lipo --task_trained qm9 --model attentivefp --percentage 0.1
 done
